5a539a101d00f6669a97895ab81b549a76d78bba
sky <568310613@qq.com>
2019-03-18 13:48:55 +0800

    first

---
+++ Tencent/pipelines.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+import pymysql
+from Tencent.settings import *
+
+
+class TencentPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+
+
